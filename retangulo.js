1d0e55313b94f8acf560e3869ae9ba0285749ec6
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-10-05 15:15:50 -0300

    Add files via upload
    7) Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever *a área do retângulo. Dica a=b.h

---
+++ retangulo.js
@@ -0,0 +1,6 @@
+function multiplique(){
+    var numero1=prompt(("digite o valor da base do retângulo:"));
+    var numero2=(prompt("digite o valor da altura do retângulo:"));
+    var multiplique=numero1*numero2;
+    alert(" A área do retângulo é :"+multiplique);
+    }
